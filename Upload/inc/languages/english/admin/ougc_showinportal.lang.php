45b9e7a7b1629c79199adc789ab01644e7a7da27
Omar G. <github@omarg.me>
2014-03-21 23:55:41 -0600

    Update requirements
    - Add MyAlerts

--- Upload/inc/languages/english/admin/ougc_showinportal.lang.php
+++ Upload/inc/languages/english/admin/ougc_showinportal.lang.php
@@ -2,27 +2,27 @@
 
 /***************************************************************************
  *
- *   OUGC Show in Portal plugin (/inc/languages/english/admin/ougc_showinportal.php)
- *	 Author: Omar Gonzalez
- *   Copyright: © 2012-2014 Omar Gonzalez
- *   
- *   Website: http://omarg.me
+ *	OUGC Show in Portal plugin (/inc/languages/english/admin/ougc_showinportal.php)
+ *	Author: Omar Gonzalez
+ *	Copyright: © 2012-2014 Omar Gonzalez
  *
- *   Choose what threads to show in portal while creating / editing.
+ *	Website: http://omarg.me
+ *
+ *	Choose what threads to show in portal while creating / editing.
  *
  ***************************************************************************
- 
+
 ****************************************************************************
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
-	
+
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ****************************************************************************/
